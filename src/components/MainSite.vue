9d5525aca0bf04116e23916d4846261bd3e7d72b
N0tail12 <N0tail12>
2022-01-25 22:29:35 +0700

    chore: get all prefectures and list out

---
+++ src/components/MainSite.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="mainSite">
+    <div class="container">
+      <div class="mainContent">
+        <h1 class="title">都道府県</h1>
+        <div class="towns-list">
+          <div class="town" v-for="town in towns" :key="town.prefCode">
+            {{ town.prefName }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import getTown from "../services/getTown";
+export default {
+  data() {
+    return {
+      towns: [],
+    };
+  },
+  mounted() {
+    getTown.fetchTown().then((data) => (this.towns = data.result));
+  },
+};
+</script>
+
+<style scoped>
+.mainSite {
+  color: #aaa;
+  font-size: 0.9rem;
+  letter-spacing: 1px;
+  font-weight: 700;
+  animation: fade-in 1s ease-in;
+}
+.mainContent {
+  padding: 20px;
+  margin: 30px;
+  max-width: 960px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: inset;
+}
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.title {
+  display: inline-block;
+}
+
+.towns-list {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  gap: 1rem;
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
